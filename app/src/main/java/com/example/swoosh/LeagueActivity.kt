c6d1ab3dc841911c2ee53807f18ca3852d2792fe
manosanjay-g <manosanjay.web@gmail.com>
2022-01-13 22:31:00 +0530

    added dummy functionality for buttons

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,11 +1,21 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LeagueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+        val nextButton = findViewById<Button>(R.id.button4)
+        nextButton.setOnClickListener {
+            nextScreen();
+        }
+    }
+    fun nextScreen(){
+        val intent = Intent(this,SkillSelectionActivity::class.java);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            nextScreen();
+        }
+    }
+    fun nextScreen(){
+        val intent = Intent(this,LeagueActivity::class.java);
+        startActivity(intent);
     }
 }
