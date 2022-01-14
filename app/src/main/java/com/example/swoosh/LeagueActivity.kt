ea6b6db3f61193b0b673209bcec3b81a5ce87ae2
manosanjay-g <manosanjay.web@gmail.com>
2022-01-14 17:47:37 +0530

    dummy app finished

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -3,9 +3,12 @@ package com.example.swoosh
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ToggleButton
+class LeagueActivity : BaseActivity() {
+    var selectedLeague:String = "";
 
-class LeagueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
@@ -17,5 +20,26 @@ class LeagueActivity : AppCompatActivity() {
     private fun nextScreen(){
         val intent = Intent(this,SkillSelectionActivity::class.java)
         startActivity(intent)
+    }
+     fun onMensClick(view: View){
+        var womenBtn = findViewById<ToggleButton>(R.id.womenLeagueBtn)
+        var mixedBtn = findViewById<ToggleButton>(R.id.mixedLeagueBtn)
+        womenBtn.isChecked = false;
+        mixedBtn.isChecked = false;
+        selectedLeague = "Mens";
+    }
+     fun onWomenClick(view: View){
+        var mensBtn = findViewById<ToggleButton>(R.id.mensLeagueBtn)
+        var mixedBtn = findViewById<ToggleButton>(R.id.mixedLeagueBtn)
+        mensBtn.isChecked = false;
+        mixedBtn.isChecked = false;
+        selectedLeague = "Women";
+    }
+     fun onMixedClick(view: View){
+        var mensBtn = findViewById<ToggleButton>(R.id.mensLeagueBtn)
+        var womenBtn = findViewById<ToggleButton>(R.id.womenLeagueBtn)
+        womenBtn.isChecked = false;
+        mensBtn.isChecked = false;
+        selectedLeague = "Mixed";
     }
 }
--- app/src/main/java/com/example/swoosh/SkillSelectionActivity.kt
+++ app/src/main/java/com/example/swoosh/SkillSelectionActivity.kt
@@ -2,10 +2,31 @@ package com.example.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import android.widget.ToggleButton
 
-class SkillSelectionActivity : AppCompatActivity() {
+class SkillSelectionActivity : BaseActivity() {
+    var selectedSkill:String = "";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill_selection)
+        val finishBtn = findViewById<Button>(R.id.finishBtn)
+        finishBtn.setOnClickListener {
+            if(selectedSkill == ""){
+                Toast.makeText(this,"Please select a skill",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    fun onBeginnerClick(view: View){
+        var proBtn = findViewById<ToggleButton>(R.id.proBtn)
+        proBtn.isChecked = false;
+        selectedSkill = "Beginner";
+    }
+    fun onProClick(view: View){
+        var beginnerBtn = findViewById<ToggleButton>(R.id.beginnerBtn)
+        beginnerBtn.isChecked = false;
+        selectedSkill = "Pro";
     }
 }
